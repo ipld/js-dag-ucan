121a458a34cc7ac79b9128883dc54e964778e4c0
Hugo Dias <hugomrdias@gmail.com>
2022-09-06 16:39:42 +0100

    chore: fix tests

--- test/lib.spec.js
+++ test/lib.spec.js
@@ -884,7 +884,7 @@ describe("api compatibility", () => {
       hasher: sha256,
     })
 
-    const { cid, bytes } = await UCAN.write(ucan)
+    const { cid, bytes } = await UCAN.write(ucan, {hasher: sha256})
     assert.deepEqual(block.cid, cid)
     assert.deepEqual(block.bytes, bytes)
     assert.deepEqual(block.value, ucan)
